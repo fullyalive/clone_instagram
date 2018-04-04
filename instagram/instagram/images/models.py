6560956e3977e410768830182f620adf49e3d75c
fullyalive <wogud1202@gmail.com>
2018-04-04 23:05:58 +0900

    Created the Like model and fixing Comment and Image models

--- instagram/instagram/images/models.py
+++ instagram/instagram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from instagram.users import models as user_models
 
 
 class TimeStampedModel(models.Model):
@@ -12,11 +13,34 @@ class TimeStampedModel(models.Model):
 
 class Image(TimeStampedModel):
 
+    """ Image Model """
+
     file = models.ImageField()
     location = models.CharField(max_length=140)
     caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
 
 
 class Comment(TimeStampedModel):
 
+    """ Comment Model """
+
     message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.message
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
