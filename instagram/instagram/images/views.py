2b4a4303dbd0331caf408930d151fec95239af61
fullyalive <wogud1202@gmail.com>
2018-04-05 20:53:49 +0900

    Creating List All Images View

--- instagram/instagram/images/views.py
+++ instagram/instagram/images/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import response
+from . import models, serializers
 
-# Create your views here.
+class ListAllImages(APIView):
+
+    def get(self, request, format=None):
+
+        all_images = models.Image.objects.all()
+
+        serializer = serializers.ImageSerializer(all_images)
+
+        return Response(data=serializer.data)
