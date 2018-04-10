d9fb6951494454bf9d4cdfdcbb69c521e2a2dfac
fullyalive <wogud1202@gmail.com>
2018-04-10 14:59:48 +0900

    Restricting LIkes

--- instagram/instagram/images/views.py
+++ instagram/instagram/images/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
 
 
@@ -37,13 +38,23 @@ class LikeImage(APIView):
         try:
             found_image = models.Image.objects.get(id=image_id)   
         except models.Image.DoesNotExist:
-            return Response(status=404)     
+            return Response(status=status.HTTP_404_NOT_FOUND)     
         
-        new_like = models.Like.objects.create(
-            creator=user,
-            image=found_image
-        )
+        try:
+           preexisting_like = models.Like.objects.get(
+               creator=user,
+               image=found_image
+           )
+           preexisting_like.delete()
 
-        new_like.save()
+           return Response(status=status.HTTP_204_NO_CONTENT)
+        except models.Like.DoesNotExist:
 
-        return Response(status=200)
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+            
+            new_like.save()
+
+        return Response(status=status.HTTP_201_CREATED)
