8c7e074ddad40faec35678640142f18c950cd154
fullyalive <wogud1202@gmail.com>
2018-04-13 18:48:17 +0900

    Refactoring the Like:Unlike

--- instagram/instagram/images/views.py
+++ instagram/instagram/images/views.py
@@ -29,35 +29,56 @@ class Feed(APIView):
 
         return Response(serializer.data)
 
+
 class LikeImage(APIView):
 
-    def get(self, request, image_id, format=None):
-        
+    def post(self, request, image_id, format=None):
+
         user = request.user
 
         try:
-            found_image = models.Image.objects.get(id=image_id)   
+            found_image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)     
-        
-        try:
-           preexisting_like = models.Like.objects.get(
-               creator=user,
-               image=found_image
-           )
-           preexisting_like.delete()
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-           return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            preexisiting_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+
         except models.Like.DoesNotExist:
 
             new_like = models.Like.objects.create(
                 creator=user,
                 image=found_image
             )
-            
+
             new_like.save()
 
-        return Response(status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
+
+
+class UnLikeImage(APIView):
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            preexisiting_like = models.Like.objects.get(
+                creator=user,
+                image__id=image_id
+            )
+            preexisiting_like.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+
 
 class CommentOnImage(APIView):
 
@@ -82,12 +103,13 @@ class CommentOnImage(APIView):
 
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class Comment(APIView):
 
-    def delete(self, request, comment_id, foremat=None):
-        
+    def delete(self, request, comment_id, format=None):
+
         user = request.user
-        
+
         try:
             comment = models.Comment.objects.get(id=comment_id, creator=user)
             comment.delete()
