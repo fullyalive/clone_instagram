8b6a3672b034dcce19a23f5b1cf297673a831bc6
fullyalive <wogud1202@gmail.com>
2018-05-01 17:12:17 +0900

    Deleting an Image

--- instagram/instagram/images/views.py
+++ instagram/instagram/images/views.py
@@ -197,6 +197,13 @@ class ModerateComments(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(image_id, user):
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format=None):
 
         user = request.user
@@ -214,12 +221,14 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-        serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
+        serializer = serializers.InputImageSerializer(
+            image, data=request.data, partial=True)
 
         if serializer.is_valid():
 
@@ -230,3 +239,17 @@ class ImageDetail(APIView):
         else:
 
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format=None):
+
+        user - request.user
+
+        image = self.find_own_image(self, image_id, user)
+
+        if image is None:
+
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
