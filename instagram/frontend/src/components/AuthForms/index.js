4bc2d6b9b846d08c80fc7971eea1771c5bc1b734
fullyalive <wogud1202@gmail.com>
2018-05-18 14:09:43 +0900

    LoginFrom

--- instagram/frontend/src/components/AuthForms/index.js
+++ instagram/frontend/src/components/AuthForms/index.js
@@ -3,36 +3,48 @@ import Ionicon from "react-ionicons";
 import styles from "./styles.scss";
 
 export const LoginForm = props => (
-  <div>
-    <form>
-      <input type="text" placeholder="아이디" />
-      <input type="password" placeholder="비밀번호" />
-      <input type="submit" value="로그인" />
+  <div className={styles.formComponent}>
+    <form className={styles.form}>
+      <input type="text" placeholder="아이디" className={styles.textInput} />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        className={styles.textInput}
+      />
+      <input type="submit" value="로그인" className={styles.button} />
     </form>
-    <span>또는</span>
-    <span>
+    <span className={styles.divider}>또는</span>
+    <span className={styles.facebookLink}>
       <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" />
       Facebook으로 로그인
     </span>
-    <span>비밀번호를 잊으셨나요?</span>
+    <span className={styles.forgotLink}>비밀번호를 잊으셨나요?</span>
   </div>
 );
 
 export const SignupForm = props => (
-  <div>
+  <div className={styles.formComponent}>
     <h3>친구들의 사진과 동영상을 보려면 가입하세요.</h3>
-    <button>
+    <button className={styles.button}>
       {" "}
       <Ionicon icon="logo-facebook" fontSize="20px" color="white" />
       Facebook으로 로그인
     </button>
-    <span>또는</span>
-    <form>
-      <input type="email" placeholder="이메일" />
-      <input type="text" placeholder="성명" />
-      <input type="username" placeholder="사용자 이름" />
-      <input type="password" placeholder="비밀번호" />
-      <input type="submit" placeholder="가입" />
+    <span className={styles.divider}>또는</span>
+    <form className={styles.form}>
+      <input type="email" placeholder="이메일" className={styles.textInput} />
+      <input type="text" placeholder="성명" className={styles.textInput} />
+      <input
+        type="username"
+        placeholder="사용자 이름"
+        className={styles.textInput}
+      />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        className={styles.textInput}
+      />
+      <input type="submit" placeholder="가입" className={styles.button} />
     </form>
     <p>
       가입하면 Instagram의 <span>약관,</span> <span>데이터 정책</span> 및{" "}
