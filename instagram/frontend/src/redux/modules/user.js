b40e00a0278ac3daed9fa1c84b72419b688e0866
fullyalive <wogud1202@gmail.com>
2018-05-20 21:23:51 +0900

    Creating Facebook Login Redux Action pt. One

--- instagram/frontend/src/redux/modules/user.js
+++ instagram/frontend/src/redux/modules/user.js
@@ -4,23 +4,48 @@
 
 // action creators
 
+// API action
+
+function facebookLogin(access_token) {
+  return function(dispatch) {
+    fetch("/users/login/facebook/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        access_token
+      })
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+  };
+}
+
 // initial state
 
-// reducer
-
 const initialState = {
-    isLoggedIn: localStorage.getItem("jwt") || false
-}
+  isLoggedIn: localStorage.getItem("jwt") || false
+};
+
+// reducer
 
 // reducer functions
 
 function reducer(state = initialState, action) {
-    switch (action.type) {
-        default:
-            return state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
 // exports
+
+const actionCreators = {
+    facebookLogin
+};
+
+export { actionCreators };
 
 // reducer export
 
