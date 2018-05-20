cdba7f972b9417cc69ee13b1dc090232614b27ba
fullyalive <wogud1202@gmail.com>
2018-05-20 19:59:20 +0900

    Login with Facebook

--- instagram/frontend/src/components/LoginForm/container.js
+++ instagram/frontend/src/components/LoginForm/container.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import LoginForm from "./presenter";
 
-const Container = props => <LoginForm {...props} />;
-
+class Container extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+  render() {
+    const { username, password } = this.state;
+    return (
+      <LoginForm
+        handleInputChange={this._handleInputChange}
+        handleSubmit={this._handleSubmit}
+        handleFacebookLogin={this._handleFacebookLogin}
+        usernameValue={username}
+        passwordValue={password}
+      />
+    );
+  }
+  _handleInputChange = event => {
+    const {
+      target: { value, name }
+    } = event;
+    this.setState({
+      [name]: value
+    });
+    console.log(this.state);
+  };
+  _handleSubmit = event => {
+    event.preventDefault();
+    // redux
+  };
+  _handleFacebookLogin = response => {
+    console.log(response);
+  };
+}
 export default Container;
--- instagram/frontend/src/components/LoginForm/presenter.js
+++ instagram/frontend/src/components/LoginForm/presenter.js
@@ -1,20 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Ionicon from "react-ionicons";
+import FacebookLogin from "react-facebook-login";
 import formStyles from "shared/formStyles.scss";
 
 const LoginForm = (props, context) => (
   <div className={formStyles.formComponent}>
-    <form className={formStyles.form}>
+    <form className={formStyles.form} onSubmit={props.handleSubmit}>
       <input
         type="text"
         placeholder={context.t("전화번호, 사용자 이름 또는 이메일")}
         className={formStyles.textInput}
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+        name="username"
       />
       <input
         type="password"
         placeholder={context.t("비밀번호")}
         className={formStyles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
+        name="password"
       />
       <input
         type="submit"
@@ -23,15 +30,28 @@ const LoginForm = (props, context) => (
       />
     </form>
     <span className={formStyles.divider}>{context.t("또는")}</span>
-    <span className={formStyles.facebookLink}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" />
-      {context.t("Facebook으로 로그인")}
-    </span>
+    <FacebookLogin
+      appId="168532063764387"
+      autoLoad={false}
+      fields="name,email,picture"
+      callback={props.handleFacebookLogin}
+      cssClass={formStyles.facebookLink}
+      icon="fa-facebook-official"
+      textButton={context.t("Facebook으로 로그인")}
+    />
     <span className={formStyles.forgotLink}>
       {context.t("비밀번호를 잊으셨나요?")}
     </span>
   </div>
 );
+
+LoginForm.propTypes = {
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleFacebookLogin: PropTypes.func.isRequired
+};
 
 LoginForm.contextTypes = {
   t: PropTypes.func.isRequired
