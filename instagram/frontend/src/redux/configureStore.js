cc5f7fb788700b7f8e9bf0ee5bddb9b06a2eac94
fullyalive <wogud1202@gmail.com>
2018-05-22 13:44:21 +0900

    Creating the photos reducer and getting the feed

--- instagram/frontend/src/redux/configureStore.js
+++ instagram/frontend/src/redux/configureStore.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
-import createHistory from "history/createBrowserHistory";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { i18nState } from "redux-i18n";
+import createHistory from "history/createBrowserHistory";
+import thunk from "redux-thunk";
 import user from "redux/modules/user";
+import photos from "redux/modules/photos";
+import { i18nState } from "redux-i18n";
 // import Reactotron from "ReactotronConfig";
 
 const env = process.env.NODE_ENV;
@@ -20,6 +21,7 @@ if (env === "development") {
 
 const reducer = combineReducers({
   user,
+  photos,
   routing: routerReducer,
   i18nState
 });
