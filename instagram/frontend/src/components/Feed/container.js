ffd2b9ebc8f9ba95041ad1380276b179e100c403
fullyalive <wogud1202@gmail.com>
2018-05-23 21:14:37 +0900

    RenderFeed Component and Persistence

--- instagram/frontend/src/components/Feed/container.js
+++ instagram/frontend/src/components/Feed/container.js
@@ -3,19 +3,34 @@ import PropTypes from "prop-types";
 import Feed from "./presenter";
 
 class Container extends Component {
-    state = {
-        loading:true
-    };
-    static propTypes = {
-        getFeed: PropTypes.func.isRequired
-    };
-    componentDidMount() {
-        const { getFeed } = this.props;
-        getFeed();
+  state = {
+    loading: true
+  };
+  static propTypes = {
+    getFeed: PropTypes.func.isRequired
+  };
+  componentDidMount() {
+    const { getFeed } = this.props;
+    if (this.props.feed) {
+      getFeed();
+    } else {
+      this.setState({
+        loading: false
+      });
     }
-    render() {
-        return <Feed {...this.state} />
+  }
+  componentWillReceiveProps = nextProps => {
+    if (nextProps.feed) {
+      this.setState({
+        loading: false
+      });
     }
+  };
+
+  render() {
+    const { feed } = this.props;
+    return <Feed {...this.state} feed={feed} />;
+  }
 }
 
 export default Container;
--- instagram/frontend/src/redux/modules/photos.js
+++ instagram/frontend/src/redux/modules/photos.js
@@ -4,7 +4,16 @@ import { actionCreators as userActions } from "redux/modules/user";
 
 // actions
 
+const SET_FEED = "SET_FEED";
+
 // action creators
+
+function setFeed(feed) {
+  return {
+    type: SET_FEED,
+    feed
+  };
+}
 
 // API actions
 
@@ -24,23 +33,33 @@ function getFeed() {
         }
         return response.json();
       })
-      .then(json => console.log(json));
+      .then(json => dispatch(setFeed(json)));
   };
 }
 // initial state
 
 const initialState = {};
 
-// reducer
+// Reducer
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    case SET_FEED:
+      return applySetFeed(state, action);
     default:
       return state;
   }
 }
 
 // reducer functions
+
+function applySetFeed(state, action) {
+  const { feed } = action;
+  return {
+    ...state,
+    feed
+  };
+}
 
 // exports
 
