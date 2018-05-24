8a62bd87b3e752e042106a4abc902a0da5eeb2a4
fullyalive <wogud1202@gmail.com>
2018-05-24 17:35:16 +0900

    Modifying className for CommentBox

--- instagram/frontend/src/components/CommentBox/presenter.js
+++ instagram/frontend/src/components/CommentBox/presenter.js
@@ -4,7 +4,7 @@ import Textarea from "react-textarea-autosize";
 import styles from "./styles.scss";
 
 const CommentBox = (props, context) => (
-  <form className={styles.commnetBox}>
+  <form className={styles.commentBox}>
     <Textarea
       className={styles.input}
       placeholder={context.t("댓글을 입력하세요...")}
