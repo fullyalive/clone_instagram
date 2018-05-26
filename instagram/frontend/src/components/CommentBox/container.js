06144cb5d82a30893a981670c9d76bdc996e0df1
fullyalive <wogud1202@gmail.com>
2018-05-27 01:26:01 +0900

    Controlling Comment Component

--- instagram/frontend/src/components/CommentBox/container.js
+++ instagram/frontend/src/components/CommentBox/container.js
@@ -2,9 +2,32 @@ import React, { Component } from "react";
 import CommentBox from "./presenter";
 
 class Container extends Component {
-    render() {
-        return <CommentBox />;
+  state = {
+    comment: ""
+  };
+  render() {
+    return (
+      <CommentBox
+        {...this.state}
+        handleInputChange={this._handleInputChange}
+        handleKeyPress={this._handleKeyPress}
+      />
+    );
+  }
+  _handleInputChange = event => {
+    const {
+      target: { value }
+    } = event;
+    this.setState({
+      comment: value
+    });
+  };
+  _handleKeyPress = event => {
+    const { key } = event;
+    if (key === "Enter") {
+      event.preventDefault();
     }
+  };
 }
 
 export default Container;
--- instagram/frontend/src/components/CommentBox/presenter.js
+++ instagram/frontend/src/components/CommentBox/presenter.js
@@ -8,6 +8,9 @@ const CommentBox = (props, context) => (
     <Textarea
       className={styles.input}
       placeholder={context.t("댓글을 입력하세요...")}
+      value={props.comment}
+      onChange={props.handleInputChange}
+      onKeyPress={props.handleKeyPress}
     />
   </form>
 );
@@ -15,5 +18,11 @@ const CommentBox = (props, context) => (
 CommentBox.contextTypes = {
   t: PropTypes.func.isRequired
 };
+
+CommentBox.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  handleKeyPress: PropTypes.func.isRequired,
+  comment: PropTypes.string.isRequired
+}
 
 export default CommentBox;
