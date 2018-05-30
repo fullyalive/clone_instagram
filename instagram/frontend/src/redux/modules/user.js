4473c0d7518ec9edf88b83eed6abd80977a33886
fullyalive <wogud1202@gmail.com>
2018-05-31 00:11:07 +0900

    Search

--- instagram/frontend/src/redux/modules/user.js
+++ instagram/frontend/src/redux/modules/user.js
@@ -8,6 +8,7 @@ const SET_USER_LIST = "SET_USER_LIST";
 const FOLLOW_USER = "FOLLOW_USER";
 const UNFOLLOW_USER = "UNFOLLOW_USER";
 const SET_EXPLORE = "SET_EXPLORE";
+const SET_IMAGE_LIST = "SET_IMAGE_LIST";
 
 // action creators
 
@@ -49,6 +50,13 @@ function setExplore(userList) {
   return {
     type: SET_EXPLORE,
     userList
+  };
+}
+
+function setImageList(imageList) {
+  return {
+    type: SET_IMAGE_LIST,
+    imageList
   };
 }
 
@@ -193,8 +201,7 @@ function getExplore() {
     const {
       user: { token }
     } = getState();
-    fetch(`/users/explore`, {
-      method: "GET",
+    fetch("/users/explore/", {
       headers: {
         Authorization: `JWT ${token}`,
         "Content-Type": "application/json"
@@ -209,6 +216,52 @@ function getExplore() {
       .then(json => dispatch(setExplore(json)));
   };
 }
+
+function searchByTerm(searchTerm) {
+  return async (dispatch, getState) => {
+    const {
+      user: { token }
+    } = getState();
+    const userList = await searchUsers(token, searchTerm);
+    const imageList = await searchImages(token, searchTerm);
+    if (userList === 401 || imageList === 401) {
+      dispatch(logout());
+    }
+    dispatch(setUserList(userList));
+    dispatch(setImageList(imageList));
+  };
+}
+
+function searchUsers(token, searchTerm) {
+  return fetch(`/users/search/?username=${searchTerm}`, {
+    headers: {
+      Authorization: `JWT ${token}`
+    }
+  })
+    .then(response => {
+      if (response.status === 401) {
+        return 401;
+      }
+      return response.json();
+    })
+    .then(json => json);
+}
+
+function searchImages(token, searchTerm) {
+  return fetch(`/users/search/?hashtags=${searchTerm}`, {
+    headers: {
+      Authorization: `JWT ${token}`
+    }
+  })
+    .then(response => {
+      if (response.status === 401) {
+        return 401;
+      }
+      return response.json();
+    })
+    .then(json => json);
+}
+
 // initial state
 
 const initialState = {
@@ -232,6 +285,8 @@ function reducer(state = initialState, action) {
       return applyUnfollowUser(state, action);
     case SET_EXPLORE:
       return applySetExplore(state, action);
+    case SET_IMAGE_LIST:
+      return applySetImageList(state, action);
     default:
       return state;
   }
@@ -296,6 +351,13 @@ function applySetExplore(state, action) {
   };
 }
 
+function applySetImageList(state, action) {
+  const { imageList } = action;
+  return {
+    ...state,
+    imageList
+  };
+}
 // exports
 
 const actionCreators = {
@@ -306,7 +368,8 @@ const actionCreators = {
   getPhotoLikes,
   followUser,
   unfollowUser,
-  getExplore
+  getExplore,
+  searchByTerm
 };
 
 export { actionCreators };
